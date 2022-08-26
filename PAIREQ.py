e045005a935d8b715e772ce7a33d71ac85832d59
uday <blackdevil190402@gmail.com>
2022-08-26 11:26:32 +0530

    added missing_alphanumericas.py

---
+++ PAIREQ.py
@@ -0,0 +1,21 @@
+
+import os
+
+def MissingCharacters(s):
+    
+    l= [0]*123
+    result= " "
+    for i in range(len(s)):
+        x= ord(s[i])
+        l[x]+=1
+    for i in range(48,58):
+        if(l[i ]==0):
+            result+=chr(i)
+            
+    for i in range(97,123):
+        if (l[i]==0):
+            result+=chr(i)
+    return result
+
+s = "12393uday"
+print(MissingCharacters(s))
--- selecting_number.py
+++ selecting_number.py
@@ -18,13 +18,10 @@ print(len(set(l)))
 for k in range(int(input())):
   l=[]
   a,b  = map(int,input().split())
-  
   for  i in range(a,b+1): 
-    
     s = str(i)
     if s.endswith("3") or s.endswith("2") or s.endswith("9") :
       l.append(s)
-      
   print(len(set(l)))
 
 
