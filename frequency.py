4d895c93ecba62fd1a50592965d7a7a99f2ab5ff
uday <blackdevil190402@gmail.com>
2022-11-17 12:29:22 +0530

    71-11

--- frequency.py
+++ frequency.py
@@ -1,14 +1,22 @@
-l = [3 ,2 ,2,1]
-s = set (l)
-d = []
-c = 0 
-for i in s:
-    j = l.count(i)
-    if c<j:
-        d.append(j)
+def f (l):
+    p = set(l)
+    h = [] 
+    for i in p:
+        h.append(l.count(i))
+        
+    s = max (h)    
+    h.remove(max(h))
+    if s in h :
+        print("NO")
+    else:
+        print("YES")
         
         
-if len(s) == len(set(d)):
-    print("True")
-else:
-    print("False")
+for i in range(int(input())):
+    n = int(input())
+    l = list(map(int, input().split()))
+    f(l)
+    
+    
+        
+        
